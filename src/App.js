baaf8862d5b72cf4c901397a5bc1faac931cd7fb
shawhien <shawhien@gmail.com>
2020-04-16 14:41:19 -0700

    App.js updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import Header from './components/Header';
+import Person from './components/Person';
+import data from './data.json';
 
-function App() {
+const App = () => {
+  const [people, setPeople] = useState(data);
+  const [likedUsers, setLikedUsers] = useState([]);
+  const [dislikedUsers, setDislikedUsers] = useState([]);
+  const activeUser = 0;
+
+  const removedPersonFromDataSrc = (peopleSource, userId) =>
+    peopleSource.filter(user => user.id !== userId);
+
+  const modifyChoices = (userId, action) => {
+    const newPeople = [...people];
+    const newLikedUsers = [...likedUsers];
+    const newSuperLikedUsers = [...superLikedUsers];
+    const newDislikedUsers = [...dislikedUsers];
+
+    switch (action) {
+      case 'ADD_TO_LIKED_USERS':
+        if (!people[activeUser].likedUsers.includes(userId)) {
+          newPeople[activeUser].likedUsers.push(userId);
+          newLikedUsers.push(data[userId]);
+
+          setLikedUsers(newLikedUsers);
+          setPeople(removedPersonFromDataSrc(people, userId));
+        }
+        break;
+      case 'ADD_TO_DISLIKED_USERS':
+        if (!people[activeUser].dislikedUsers.includes(userId)) {
+          newPeople[activeUser].dislikedUsers.push(userId);
+          newDislikedUsers.push(data[userId]);
+
+          setDislikedUsers(newLikedUsers);
+          setPeople(removedPersonFromDataSrc(people, userId));
+        }
+        break;
+      default:
+        return people;
+    }
+  };
+
   return (
-    <p>Barks Joy!</p>
+    <div className="app">
+      <Header />
+      {people[1] ? (
+        <Person
+          key={people[1].id}
+          person={people[1]}
+          modifyChoices={modifyChoices}
+          likedUsers={likedUsers}
+        />
+      ) : (
+        <Lonely
+          activeUserImage={people[activeUser].image}
+          likedUsers={likedUsers}
+        />
+      )}
+    </div>
   );
-}
+};
 
 export default App;
