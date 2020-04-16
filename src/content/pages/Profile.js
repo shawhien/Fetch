aa2cb3ae416c3145d71514dc3ad1451032758300
micajank <micajank@umich.edu>
2020-04-16 14:38:48 -0700

    Initial file setup and JSON file created

---
+++ src/content/pages/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+import ProfileInfo from '../json/JSONContent.json';
+
+export default function Profile(props) {
+
+    return (
+        <div>
+            <h1>ProfileInfo[0].humanInfo.name</h1>
+        </div>
+    )
+}
